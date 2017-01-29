0d3c0aa312c507521d68a8cbe4b4bc5f3103f5b0
antipenko <sweetglory.victory@gmail.com>
2017-01-29 14:01:42 +0200

    add subscribe and instagram

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,6 +4,45 @@ $(document).foundation();
       $('.ps-slider-post').slick({
         
       });
+
+       $('.ps-instagram').slick({
+    		slidesToShow: 8,
+    		slidesToScroll: 1,
+    		autoplay: true,
+  			autoplaySpeed: 1000,
+  			variableWidth: true,
+  			responsive: [
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 8,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 1368,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },{
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
     });
 
  
